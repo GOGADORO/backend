1642185e96f718116a70805765d4dad358853187
GOGADORO <45120083+GOGADORO@users.noreply.github.com>
2021-07-09 17:29:19 +0900

    Update backend.js

--- backend.js
+++ backend.js
@@ -4,17 +4,17 @@ const { PythonShell } = require('python-shell')
 //post방식으로 데이터를 받을 때 필요한 모듈입니다.
 //req에 데이터를 담아줍니다.
 
-app.get('',(req,res)=>{
-    res.sendFile(__dirname + '/index.html')
-})
-
-
 app.get("/",function(req,res){
 let options = {
   mode: 'text',
   pythonOptions: ['-u'], // get print results in real-time
   args: req.param('stations') // Python Script에 넘겨줄 인자 목록
 };
+
+app.get('/metro',(req,res)=>{
+    res.sendFile(__dirname + '/index.html')
+})
+
 console.log("stations :", req.param('stations'));
 PythonShell.run('./test.py', options, function(err, msg) {
 	if (err) throw err;
